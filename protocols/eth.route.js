9d9f356f7d111dab897e1e52126e0ffe91290f34
Michael Feng <mifeng@gmail.com>
2020-09-28 18:30:43 -0700

    (feat) approve and getERC20Approval functions

--- protocols/eth.route.js
+++ protocols/eth.route.js
@@ -41,7 +41,21 @@ const getERC20Balance = async (wallet, tokenAddress) => {
   return balance/1e18.toString()
 }
 
-router.get('/get-balances', async (req, res) => {
+// get ERC-20 token allowance
+const getERC20Allowance = async (wallet, spender, tokenAddress) => {
+  // instantiate a contract and pass in provider for read-only access
+  const contract = new ethers.Contract(tokenAddress, utils.ERC20Abi, provider)
+  const allowance = await contract.allowance(wallet.address, spender)
+  return allowance/1e18.toString()
+}
+
+const approveERC20 = async (spender, tokenAddress, amount) => {
+  // instantiate a contract and pass in wallet, which act on behalf of that signer
+  const contract = new ethers.Contract(tokenAddress, utils.ERC20Abi, wallet)
+  return await contract.approve(spender, amount)
+}
+
+router.get('/balances', async (req, res) => {
   const initTime = Date.now()
   const privateKey = "0x" + process.env.ETH_PRIVATE_KEY // replace by passing this in as param
   const wallet = new ethers.Wallet(privateKey, provider)
@@ -62,19 +76,72 @@ router.get('/get-balances', async (req, res) => {
   )
 })
 
+router.get('/allowances', async (req, res) => {
+  const initTime = Date.now()
+  const privateKey = "0x" + process.env.ETH_PRIVATE_KEY // replace by passing this in as param
+  const wallet = new ethers.Wallet(privateKey, provider)
+
+  // params: spender (required)
+  const spender = req.query.spender
+
+  const approvals = {}
+  Promise.all(
+    Object.keys(erc20_tokens).map(async (key) =>
+    approvals[key] = await getERC20Allowance(wallet, spender, erc20_tokens[key])
+    )).then(() => {
+      res.status(200).json({
+        network: network,
+        timestamp: initTime,
+        latency: utils.latency(initTime, Date.now()),
+        spender: spender,
+        approvals: approvals,
+      })
+    }
+  )
+})
+
+router.get('/approve', async (req, res) => {
+  const initTime = Date.now()
+  const privateKey = "0x" + process.env.ETH_PRIVATE_KEY // replace by passing this in as param
+  const wallet = new ethers.Wallet(privateKey, provider)
+
+  // params: symbol (required), spender (required), amount (optional)
+  const symbol = req.query.symbol
+  const spender = req.query.spender
+  let amount
+  req.query.amount  ? amount = ethers.utils.parseEther(req.query.amount)
+                    : amount = ethers.utils.parseEther('1000000000') // approve for 1 billion units if no amount specified
+  const tokenAddress = erc20_tokens[symbol]
+
+  // call approve function
+  const approval = await approveERC20(wallet, tokenAddress, amount)
+
+  // submit response
+  res.status(200).json({
+    network: network,
+    timestamp: initTime,
+    latency: utils.latency(initTime, Date.now()),
+    symbol: symbol,
+    spender: spender,
+    amount: amount,
+    approval: approval,
+  })
+})
+
 // Faucet to get test tokens
 router.get('/faucet', (req, res) => {
   const initTime = Date.now()
   const privateKey = "0x" + process.env.ETH_PRIVATE_KEY // replace by passing this in as param
   const wallet = new ethers.Wallet(privateKey, provider)
 
-  let tokenSymbol
-  req.query.symbol  ? tokenSymbol = req.query.symbol
-                    : tokenSymbol = "WETH"
+  // params: symbol (optional)
+  let symbol
+  req.query.symbol  ? symbol = req.query.symbol
+                    : symbol = "WETH"
 
   // Deposit Kovan ETH to get Kovan WETH
   const wei = ethers.utils.parseEther("0.3")
-  const tokenAddress = erc20_tokens[tokenSymbol]
+  const tokenAddress = erc20_tokens[symbol]
   // instantiate a contract and pass in wallet, which act on behalf of that signer
   const contract = new ethers.Contract(tokenAddress, utils.KovanWETHAbi, wallet)
   contract.deposit({value: wei}).then((response) => {
