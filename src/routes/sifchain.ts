93b02d148bc9a5845d35e4cccbb6e65fd5b4eaa8
kol845 <pebo-hamza@hotmail.com>
2021-11-07 20:00:03 +0100

    Working of /sifchain/price endpoint

--- src/routes/sifchain.ts
+++ src/routes/sifchain.ts
@@ -1,21 +1,22 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { NetworkApi, Configuration } from '../../sifchain';
 import Sifchain from '../services/sifchain';
+import { latency, statusMessages } from '../services/utils';
+import { logger } from '../services/logger';
 
 const router = express.Router();
 
 const sifchain: Sifchain = new Sifchain();
 
-// constants
-const config = new Configuration({ basePath: sifchain.getBasePath() });
-const networkApi = new NetworkApi(config);
+/*
+Get Sifchain network and configuration information
+*/
 router.post('/', async (_req: Request, res: Response) => {
   /*
     POST /
   */
-  const networkInfoRes = await networkApi.getNetworkInfo();
-
+  const networkInfoRes = await sifchain.getNetworkInfo();
+  const config = sifchain.getConfig();
   res.status(200).json({
     config,
     networkInfo: networkInfoRes.data,
@@ -23,6 +24,120 @@ router.post('/', async (_req: Request, res: Response) => {
     connection: true,
     timestamp: Date.now(),
   });
+});
+
+/*
+Get prices of two tokens:
+base & quote
+*/
+router.post('/price', async (req, res) => {
+  /*
+    POST:
+    x-www-form-urlencoded: {
+      "base":"ROWAN"
+      "quote":"ATOM"
+      "side":"buy" or "sell"
+      "amount":1
+    }
+  */
+  const initTime = Date.now();
+
+  const baseToken = req.body.base;
+  const quoteToken = req.body.quote;
+  const tradeType = req.body.side.toUpperCase();
+  const amount = parseFloat(req.body.amount);
+
+  try {
+    const exchangeRate = await sifchain.getSwapRate(
+      baseToken,
+      quoteToken,
+      amount,
+      tradeType
+    );
+
+    if (typeof exchangeRate === 'string') {
+      res.status(500).json({
+        error: 'Unable to getSwapRate',
+      });
+    } else {
+      res.status(200).json({
+        timestamp: initTime,
+        latency: latency(initTime, Date.now()),
+        base: baseToken,
+        quote: quoteToken,
+        amount: amount,
+        tradeType: tradeType,
+        ...exchangeRate,
+      });
+    }
+  } catch (err) {
+    logger.error(req.originalUrl, { message: err });
+    let message;
+    let reason;
+    err.reason
+      ? (reason = err.reason)
+      : (reason = statusMessages.operation_error);
+    const isAxiosError = err.isAxiosError;
+    if (isAxiosError) {
+      reason = err.response.status;
+      message = err.response.statusText;
+    } else {
+      message = err;
+    }
+    res.status(500).json({
+      error: reason,
+      message: message,
+    });
+  }
+});
+
+/*
+Get Pool information for 'token'
+*/
+router.post('/pool', async (req, res) => {
+  /*
+    POST:
+    x-www-form-urlencoded: {
+      "token":"ATOM"
+    }
+  */
+  const initTime = Date.now();
+
+  const token = req.body.token;
+
+  try {
+    const poolData = await sifchain.getPool(token);
+
+    if (typeof poolData === 'string') {
+      res.status(500).json({
+        error: 'Unable to getSwapRate',
+      });
+    } else {
+      res.status(200).json({
+        timestamp: initTime,
+        latency: latency(initTime, Date.now()),
+        ...poolData,
+      });
+    }
+  } catch (err) {
+    logger.error(req.originalUrl, { message: err });
+    let message;
+    let reason;
+    err.reason
+      ? (reason = err.reason)
+      : (reason = statusMessages.operation_error);
+    const isAxiosError = err.isAxiosError;
+    if (isAxiosError) {
+      reason = err.response.status;
+      message = err.response.statusText;
+    } else {
+      message = err;
+    }
+    res.status(500).json({
+      error: reason,
+      message: message,
+    });
+  }
 });
 
 export default router;
--- src/services/sifchain.ts
+++ src/services/sifchain.ts
@@ -1,20 +1,73 @@
 const globalConfig =
   require('../services/configuration_manager').configManagerInstance;
 
+import { PoolsApi, AssetsApi, Configuration, NetworkApi } from '../../sifchain';
+
 // constants
 
 export default class Sifchain {
   private lcdUrl;
   private basePath;
+  private config;
+  private networkApi;
+  private assetsApi;
+  private poolsApi;
 
   constructor() {
     this.lcdUrl = globalConfig.getConfig('SIFCHAIN_LCD_URL');
     this.basePath = globalConfig.getConfig('SIFCHAIN_BASE_PATH');
+    this.config = new Configuration({ basePath: this.basePath });
+    this.networkApi = new NetworkApi(this.config);
+    this.assetsApi = new AssetsApi(this.config);
+    this.poolsApi = new PoolsApi(this.config);
   }
   getLCDURL(): string {
     return this.lcdUrl;
   }
   getBasePath(): string {
     return this.basePath;
+  }
+  getConfig(): any {
+    return this.config;
+  }
+  async getNetworkInfo(): Promise<any> {
+    return this.networkApi.getNetworkInfo();
+  }
+  // get Sifchain Swap Rate
+  async getSwapRate(
+    baseToken: string,
+    quoteToken: string,
+    amount: number,
+    tradeType: string
+  ): Promise<any> {
+    try {
+      const basePriceRes = await this.assetsApi.getTokenValue(baseToken);
+      const quotePriceRes = await this.assetsApi.getTokenValue(quoteToken);
+
+      const basePriceData = basePriceRes.data;
+      const quotePriceData = quotePriceRes.data;
+      return {
+        basePriceData,
+        quotePriceData,
+        amount,
+        tradeType,
+      };
+    } catch (err) {
+      console.log(err);
+      return 'failed';
+    }
+  }
+  // get Sifchain Pool
+  async getPool(token: string): Promise<any> {
+    try {
+      const poolRes = await this.poolsApi.getPool(token);
+      const poolData = poolRes.data;
+      return {
+        poolData,
+      };
+    } catch (err) {
+      console.log(err);
+      return 'failed';
+    }
   }
 }
