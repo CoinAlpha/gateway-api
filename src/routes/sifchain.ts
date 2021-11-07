b3066a290644a9be90884cd6be5a594299ca0f06
kol845 <pebo-hamza@hotmail.com>
2021-11-07 15:53:24 +0100

    Network data fetch

--- src/routes/sifchain.ts
+++ src/routes/sifchain.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { Configuration } from '../../sifchain';
+import { NetworkApi, Configuration } from '../../sifchain';
 import Sifchain from '../services/sifchain';
 
 const router = express.Router();
@@ -8,13 +8,17 @@ const router = express.Router();
 const sifchain: Sifchain = new Sifchain();
 
 // constants
-const config = new Configuration({ basePath: process.env.REST_API });
+const config = new Configuration({ basePath: sifchain.getBasePath() });
+const networkApi = new NetworkApi(config);
 router.post('/', async (_req: Request, res: Response) => {
   /*
     POST /
   */
+  const networkInfoRes = await networkApi.getNetworkInfo();
+
   res.status(200).json({
     config,
+    networkInfo: networkInfoRes.data,
     lcdUrl: sifchain.getLCDURL(),
     connection: true,
     timestamp: Date.now(),
--- src/services/sifchain.ts
+++ src/services/sifchain.ts
@@ -5,11 +5,16 @@ const globalConfig =
 
 export default class Sifchain {
   private lcdUrl;
+  private basePath;
 
   constructor() {
     this.lcdUrl = globalConfig.getConfig('SIFCHAIN_LCD_URL');
+    this.basePath = globalConfig.getConfig('SIFCHAIN_BASE_PATH');
   }
   getLCDURL(): string {
     return this.lcdUrl;
+  }
+  getBasePath(): string {
+    return this.basePath;
   }
 }
